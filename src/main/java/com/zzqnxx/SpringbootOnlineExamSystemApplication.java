f2979d872a772bf0434287af5a84b794eb9eff2e
gdufeZLYL <18027894043@163.com>
2017-12-21 20:30:50 +0800

    first commit

---
+++ src/main/java/com/zzqnxx/SpringbootOnlineExamSystemApplication.java
@@ -0,0 +1,12 @@
+package com.zzqnxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOnlineExamSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOnlineExamSystemApplication.class, args);
+	}
+}
